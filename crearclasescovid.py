79e86567ddb554a13eee1a652a95a429924b0c44
Andres8910 <ivan.ayb@hotmail.com>
2020-05-19 00:25:53 +0000

    Agregue dos objetos para la clase Incidente y importe Datetime

--- crearclasescovid.py
+++ crearclasescovid.py
@@ -1,13 +1,16 @@
+import datetime #importo una funcion para que pueda tener fechas
+
 #Aqui declare una
 #clase llamada Pais
 class Pais:  #---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS#
     # El __init__ es un constructor , que es un metodo y sirve para generar objetos parametrizados
 
-    def __init__(self, NombreING, NombreESP, Fallecidos, Contagiados):
-        self.NombreING = NombreING
-        self.NombreESP = NombreESP
-        self.Fallecidos = Fallecidos
-        self.Contagiados = Contagiados
+    def __init__(self, NombreING, NombreESP, Fallecidos, Contagiados, PDC):
+        self.NombreING = NombreING #String
+        self.NombreESP = NombreESP #String
+        self.Fallecidos = Fallecidos #int
+        self.Contagiados = Contagiados #int
+        self.PDC = PDC #Tipo datetime
 
 
 #Aqui declare otra clase llamada incidente
@@ -15,12 +18,19 @@ class Pais:  #---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATR
 
 class Incidente:
     def __init__(self, Fecha, Pais, NContagios, NFallecidos):
-        self.Fecha = Fecha
-        self.Pais = Pais
-        self.NContagios = NContagios
-        self.NFallecidos = NFallecidos
+        self.Fecha = Fecha  #Tipo datetime
+        self.Pais = Pais     #String
+        self.NContagios = NContagios #int
+        self.NFallecidos = NFallecidos #int
 
 
-MiPais = Pais('Mexico','México',1000,5000)
-print(MiPais.NombreING)
+#Aqui hice un objeto para la clase Pais
+MiPais = Pais('Mexico','México',1000,5000,datetime.datetime(2020, 5, 18))
+print(MiPais.PDC)
 
+#Aqui hice dos objetos para la clase Incidente
+
+IncidenteHoy = Incidente(datetime.datetime.now(),'Mexico',1500,18)
+print(IncidenteHoy.Fecha)
+IncidenteAyer = Incidente(datetime.datetime(2020, 5, 17),'Mexico',800,10)
+print(IncidenteAyer.Fecha)
