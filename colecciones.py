f84161a7eeb4ffb43755c70ad828adfbe7926fe1
Andres8910 <ivan.ayb@hotmail.com>
2020-05-20 03:14:29 +0000

    En el programa Colecciones.py. Hice el Reto de instanciar cinco paises en una lista llamada mis_paises y mandarlos a llamar

--- colecciones.py
+++ colecciones.py
@@ -24,10 +24,43 @@ mi_lista.append(IncidenteAyer)
 # el len extrae la cantidad de elementos en la lista que son IncidenteHoy y IncidenteAyer
 print(len(mi_lista))
 
+#=================================================================================
+
 #se imprime con un bucle for los objetos que estan en la lista
 for cont in mi_lista:
   print(cont.Pais)
 
+class Pais:  #---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS---ATRIBUTOS#
+    # El __init__ es un constructor , que es un metodo y sirve para generar objetos parametrizados
+
+    def __init__(self, NombreING, NombreESP, Fallecidos, Contagiados, PDC):
+        self.NombreING = NombreING #String
+        self.NombreESP = NombreESP #String
+        self.Fallecidos = Fallecidos #int
+        self.Contagiados = Contagiados #int
+        self.PDC = PDC #Tipo datetime
 
 
+#========================================================================
+#Reto Hacer 5 obejtos y isntanciar 5 paises en una lista 
 
+Germany = Pais('Germany','Alemania',5000,15000,datetime.datetime(2020, 4, 11))
+Canada = Pais('Canada','Canadá',2500,7800,datetime.datetime(2020, 5, 11))
+China = Pais('China','China',100000,250000,datetime.datetime(2020, 2, 1))
+Russia = Pais('Russia','Rusia',700,2000,datetime.datetime.now())
+Brazil = Pais('Brazil','Brasil',10000,30000,datetime.datetime(2020, 3, 3))
+
+mis_paises = []
+
+mis_paises.append(Germany)
+mis_paises.append(Canada)
+mis_paises.append(China)
+mis_paises.append(Russia)
+mis_paises.append(Brazil)
+
+print("================================")
+
+print(len(mis_paises))
+
+for contador in mis_paises:
+  print(contador.NombreING)
